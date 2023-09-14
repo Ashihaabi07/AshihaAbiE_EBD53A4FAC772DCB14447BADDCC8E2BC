9cdaf4cb2e27eaa7314b3c3af69267cb04908a4d
Ashihaabi07 <139944125+Ashihaabi07@users.noreply.github.com>
2023-09-14 16:10:32 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,42 @@
-def factorial(n):
-  if n == 0:
-    return 1
-  else:
-    return n * factorial(n - 1)
+class BankAccount:
+
+  def __init__(self, account_number, account_holder_name, initial_balance=0.0):
+    self.__account_number = account_number
+    self.__account_holder_name = account_holder_name
+    self.__account_balance = initial_balance
+
+  def deposit(self, amount):
+    if amount > 0:
+      self.__account_balance += amount
+      # self.__account_balance = self.__account_balance+amount
+      print("Deposited ₹{}. New balance: ₹{}".format(amount,
+                                                     self.__account_balance))
+    else:
+      print("Invalid deposit amount.")
+
+  def withdraw(self, amount):
+    if amount > 0 and amount <= self.__account_balance:
+      self.__account_balance -= amount
+      # self.__account_balance = self.__account_balance - amount
+      print("Withdrew ₹{}. New balance: ₹{}".format(amount,
+                                                    self.__account_balance))
+    else:
+      print("Invalid withdrawal amount or insufficient balance.")
+
+  def display_balance(self):
+    print("Account balance for {} (Account #{}): ₹{}".format(
+        self.__account_holder_name, self.__account_number,
+        self.__account_balance))
 
 
-num = int(input("Enter a number: "))
-if num < 0:
-  print("Factorial is not defined for negative numbers.")
-else:
-  result = factorial(num)
-  print(f"The factorial of {num} is {result}")
+# Create an instance of the BankAccount class
+account = BankAccount(account_number="123456789",
+                      account_holder_name="Hari Prabu",
+                      initial_balance=5000.0)
+
+# Test deposit and withdrawal functionality
+account.display_balance()
+account.deposit(500.0)
+account.withdraw(200.0)
+account.withdraw(20000.0)
+account.display_balance()
